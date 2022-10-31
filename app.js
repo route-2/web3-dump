e832a6a4763c5907a80e118b88b215e68c810838
route-2 <ruthurao@gmail.com>
2022-11-01 00:35:39 +0530

    trials

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-var Tx = require('ethereumjs-tx')
+
+
+var Tx = require('ethereumjs-tx').Transaction
 var Web3 = require('web3')
+const env = require('dotenv').config()
+
 const web3 = new Web3('https://goerli.infura.io/v3/')
 
-const account1 = '0x657dD02b4EAE6Bf276747bd3B9E54aB034D9F97f' 
-const account2 = '0xD6914c60FE71Dad50CabaF3070eBfc6118398810'
+const account1 = '' 
+const account2 = ''
 
-const privateKey1 = Buffer.from(process.env.PRIVATE_KEY_1)
-const privateKey2 = Buffer.from(process.env.PRIVATE_KEY_2)
+const privateKey1 = Buffer.from(process.env.PRIVATE_KEY_1,'hex')
+const privateKey2 = Buffer.from(process.env.PRIVATE_KEY_2,'hex')
 
-
+console.log(privateKey1)
 web3.eth.getTransactionCount(account1, (err, txCount) => {
   
   const txObject = {
@@ -18,7 +22,7 @@ web3.eth.getTransactionCount(account1, (err, txCount) => {
     gasLimit: web3.utils.toHex(21000),
     gasPrice: web3.utils.toHex(web3.utils.toWei('10', 'gwei'))
   }
-    const tx = new Tx(txObject)
+   const tx = new Tx(txObject,{chain:'goerli'})
   tx.sign(privateKey1)
 
   const serializedTx = tx.serialize()
