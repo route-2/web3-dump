f5118026e6f4720361261dce193929c632ac0a11
route-2 <ruthurao@gmail.com>
2022-11-06 00:56:09 +0530

    store

--- 5.js
+++ 5.js
@@ -9,11 +9,50 @@ const account2 = ''
 
 const privateKey1 = Buffer.from(process.env.PRIVATE_KEY_1,'hex')
 const privateKey2 = Buffer.from(process.env.PRIVATE_KEY_2,'hex')
+const contractAddress = ''
+const contractABI = ''
 
-const txObject = {
-    nonce:    web3.utils.toHex(txCount),
-    gasLimit: web3.utils.toHex(800000),
-    gasPrice: web3.utils.toHex(web3.utils.toWei('10', 'gwei')),
-    to: contractAddress,
-    data: data
-  }
+const contract = new web3.eth.Contract(contractABI, contractAddress)
+
+
+
+
+  console.log(privateKey1)
+  web3.eth.getTransactionCount(account1, (err, txCount) => {
+
+    const txObject1 = {
+        nonce:    web3.utils.toHex(txCount),
+        gasLimit: web3.utils.toHex(800000), 
+        gasPrice: web3.utils.toHex(web3.utils.toWei('10', 'gwei')),
+        to: contractAddress,
+        data: contract.methods.transfer(account2, 1000).encodeABI()
+      }
+      const txObject = {
+        nonce:    web3.utils.toHex(txCount),
+        gasLimit: web3.utils.toHex(800000), 
+        gasPrice: web3.utils.toHex(web3.utils.toWei('10', 'gwei')),
+        to: contractAddress,
+        data: contract.methods.transfer(account2, 1000).encodeABI()
+      }
+  
+    const tx = new Tx(txObject)
+    tx.sign(privateKey1)
+  
+    const serializedTx = tx.serialize()
+    const raw = '0x' + serializedTx.toString('hex')
+  
+    web3.eth.sendSignedTransaction(raw, (err, txHash) => {
+      console.log('err:', err, 'txHash:', txHash)
+     
+    })
+  })
+  
+  
+  contract.methods.balanceOf(account1).call((err, balance) => {
+    console.log({ err, balance })
+  })
+  
+ 
+  contract.methods.balanceOf(account2).call((err, balance) => {
+    console.log({ err, balance })
+  })
