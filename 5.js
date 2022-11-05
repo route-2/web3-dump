9dac8cc47683ade981b814b4c8e6b8d418c714a6
route-2 <ruthurao@gmail.com>
2022-11-06 01:16:01 +0530

    store

--- 5.js
+++ 5.js
@@ -2,10 +2,10 @@ var Tx = require('ethereumjs-tx').Transaction
 var Web3 = require('web3')
 const env = require('dotenv').config()
 
-const web3 = new Web3('https://eth-goerli.g.alchemy.com/v2/gh4d1-dAT4B_1Khy86s7JUbFhQIclYqO')
+const web3 = new Web3('https://eth-goerli.g.alchemy.com/v2/')
 
-const account1 = '0x657dD02b4EAE6Bf276747bd3B9E54aB034D9F97f' 
-const account2 = '0xD6914c60FE71Dad50CabaF3070eBfc6118398810'
+const account1 = '' 
+const account2 = ''
 
 const privateKey1 = Buffer.from(process.env.PRIVATE_KEY_1,'hex')
 const privateKey2 = Buffer.from(process.env.PRIVATE_KEY_2,'hex')
